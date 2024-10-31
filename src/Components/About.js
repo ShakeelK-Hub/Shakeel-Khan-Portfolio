75340daf911aac79e62bbe19d0448b1e0e38c3eb
Shakeel Khan <601572@student.belgiumcampus.ac.za>
2024-10-31 12:15:24 +0200

    Modified changes to the Abput section and added links to my social media pages

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,43 +11,54 @@ function About() {
 
       <h2 className='about-title'>About</h2>
 
+      
+
+        <ul className='about-items'>
+    <li className='about-item' >
+        <img src={img2} alt='cursor' /> 
+        <div className='about-text'>
+            <h3 className='about-item-title'>Front End Developer</h3>
+            <p>I am a front end Developer</p>
+        </div>
+    </li>
+
+    <li className='about-item' >
+        <img src={img3} alt='server' />
+        <div className='about-text'>
+            <h3 className='about-item-title'>Backend Developer</h3>
+            <p>I am a backend Developer</p>
+        </div>
+    </li>
+
+    <li className='about-item' >
+        <img src={img4} alt='User interface' />
+        <div className='about-text'>
+            <h3 className='about-item-title'>UI Designer</h3>
+            <p>I am a UI Designer</p>
+        </div>
+    </li>
+</ul>
+
+
+ 
+
+
       <div className='about-content'>
         <img src={img1} alt='Shakeel' className='about-img' />
 
-        <ul className='about-items'>
+        <ul className='about-s'>
+    <li className='about-ss' title='I create responsive, dynamic, and user-focused front-end experiences using HTML, CSS, JavaScript, and React.'>
+        
+        <div className='about-intro'>
+            <h3 className='about-para'>SHAKEEL KHAN</h3>
+            <p>In my journey through software development, I have built a strong foundation in both theoretical concepts and practical application. My studies have focused on mastering core programming languages, such as C# and JavaScript, and exploring areas like web development, database management, and software engineering principles. Through hands-on projects, I’ve gained experience in the full software development lifecycle, from planning and designing to coding and testing. My academic work has also taught me to approach challenges logically and develop solutions that are both efficient and user-centered, skills that I’m eager to apply in real-world settings.</p>
+        </div>
+    </li>
+    </ul>
 
-          <li className='about-item'>
-            <img src={img2} alt='cursor' />
 
-            <div className='about-text'>
-              <h3>Front End Developer</h3>
-              <p>I am a front end Developer</p>
-            </div>
 
-          </li>
-
-          <li className='about-item'>
-            <img src={img3} alt='server' />
-
-            <div className='about-text'>
-              <h3>Backend Developer</h3>
-              <p>I am a backend Developer</p>
-            </div>
-
-          </li>
-
-          <li className='about-item'>
-            <img src={img4} alt='User interface' />
-
-            <div className='about-text'>
-              <h3>UI Designer</h3>
-              <p>I am a backend Developer</p>
-            </div>
-
-          </li>
-
-        </ul>
-
+        
       </div>
       
     </section>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,24 +8,49 @@ function Hero() {
     <section className='hero-container'>
 
       <div className='hero-content'>
-        
+
+
+        <h4 className='subtit'>Student Software Developer</h4>
         <h1 className='hero-title'>Hi, I’m Shakeel</h1>
 
         <p className='hero-description'>
-          An IT student with a passion for technology and continuous learning, 
-          eager to contribute to the ever-changing world and seek opportunities to use skills in a dynamic IT environment.
+         Utilizing code to build visually stunning platforms.<br /><br />
+         A passionate and dedicated student of software development. With a keen eye for detail and a drive for excellence, I am committed to crafting elegant and efficient code. My journey in software development is fueled by a love for problem-solving and a desire to create innovative solutions that make a difference.
           <br /><br />
-          Reach out if you would like to learn more!
+         
+          Let’s connect and explore the endless possibilities of technology together!
         </p>
 
         <div className="hero-button-container">
-          <a href='mailto:601572@student.belgiumcampus.ac.za' className='hero-contact-btn'>Contact Me</a>
+          
           <a href={CV} download='CV' className='hero-contact-btn'>Download CV</a>
+          <a href='mailto:601572@student.belgiumcampus.ac.za' className='hero-contact-btn'>Contact Me</a>
         </div>
 
       </div>
 
+      <div className='hero-image-container'>
+        
       <img src={img1} alt='hero' className='hero-img' />
+
+      <div className='hero-icons'>
+
+        <a href='https://github.com/ShakeelK-Hub' target='_blank' rel='noopener noreferrer'>
+          <i className='fab fa-github'></i>
+        </a>
+
+        <a href='mailto:601572@student.belgiumcampus.ac.za'>
+          <i className='fas fa-envelope'></i>
+        </a>
+
+        <a href='https://www.linkedin.com/in/shakeel-khan-b14264231/' target='_blank' rel='noopener noreferrer'>
+          <i className='fab fa-linkedin'></i>
+        </a>
+        
+      </div>
+      
+      </div>
+
 
       <div className='hero-topcircle' />
       <div className='hero-bottomcircle' />
